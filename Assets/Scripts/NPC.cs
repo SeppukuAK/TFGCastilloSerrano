f133bd0a7725fe03a23712ee6d8684d231a5131e
Raúl Serrano Gómez <raulserranodev@gmail.com>
2020-03-03 11:43:44 +0100

    Subido proyecto a GitHub

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using BehaviorDesigner.Runtime;
+
+namespace TFG
+{
+    public class NPC : MonoBehaviour
+    {
+        public VRTK_SDKManager SDKManager;
+
+        private Transform headTransform;
+        private BehaviorTree behaviorTree;
+
+
+        private void Awake()
+        {
+            behaviorTree = GetComponent<BehaviorTree>();
+            VRTK_SDKManager.instance.AddBehaviourToToggleOnLoadedSetupChange(this);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            headTransform = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset);
+
+            SharedGameObject headSharedGameObject = headTransform.gameObject;
+            behaviorTree.SetVariable("Head", headSharedGameObject);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
