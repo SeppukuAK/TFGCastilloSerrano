20490d7ad87a059292a9cd64e31145e82f9d5370
Raúl Serrano Gómez <raulserranodev@gmail.com>
2020-03-05 11:07:04 +0100

    Intentando montar estructura de animaciones

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -6,6 +6,7 @@ using BehaviorDesigner.Runtime;
 
 namespace TFG
 {
+    [RequireComponent(typeof(Animator))]
     public class NPC : MonoBehaviour
     {
         public VRTK_SDKManager SDKManager;
@@ -13,10 +14,24 @@ namespace TFG
         private Transform headTransform;
         private BehaviorTree behaviorTree;
 
-
+    
         private void Awake()
         {
             behaviorTree = GetComponent<BehaviorTree>();//Obtenemos el árbol de comportamiento
+
+            Animator animator = GetComponent<Animator>();
+
+            //https://docs.unity3d.com/ScriptReference/Animations.AnimatorController.html?_ga=2.239912643.1726044905.1583340668-154422748.1555576536
+            if (animator.runtimeAnimatorController != null)
+            {
+                Debug.LogError("No debe haber un controlador asociado al animator");
+                animator.runtimeAnimatorController = null;
+            }
+
+            // Creates the controller
+            var controller = UnityEditor.Animations.AnimatorController.CreateAnimatorControllerAtPath("Assets/Adri.controller");
+            DestroyImmediate(controller,true);
+            //animator.runtimeAnimatorController = controller;
 
             VRTK_SDKManager.instance.AddBehaviourToToggleOnLoadedSetupChange(this);
         }
@@ -29,6 +44,7 @@ namespace TFG
             SharedGameObject headSharedGameObject = headTransform.gameObject;
             behaviorTree.SetVariable("Head", headSharedGameObject);
             behaviorTree.EnableBehavior();//Se activa el arbol de comportamiento
+
 
         }
 
