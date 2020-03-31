f368e8d3b4a47034011eac20da14baca09823609
Adriana <Adridelcastillodev@gmail.com>
2020-03-31 12:05:25 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/IsPlayerLookingAtNPC.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsPlayerLookingAtNPC : MonoBehaviour
+{
+    private Transform headTransform;
+
+    void Start()
+    {
+        headTransform = Camera.main.transform;//Obtenemos el headset activo
+    }
+
+
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        //Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+
+}
