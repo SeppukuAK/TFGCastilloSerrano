1148912e654892fce33a1925ccf4e14ea6206a0c
Raúl Serrano Gómez <raulserranodev@gmail.com>
2020-04-23 11:36:31 +0200

    Empezzando merge

---
+++ Assets/Scripts/Behavior Designer Social Presence/Tasks/Conditionals/Environment Interaction/Has/HasBeenHitByInteractable.cs
@@ -0,0 +1,31 @@
+﻿using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace TFG
+{
+    [TaskDescription("Devuelve si el NPC ha sido golpeado por un objeto interactuable")]
+    [TaskCategory("TFG")]
+    public class HasBeenHitByInteractable : Conditional
+    {
+        private bool enteredCollision = false;
+
+        public override TaskStatus OnUpdate()
+        {
+            return enteredCollision ? TaskStatus.Success : TaskStatus.Failure;
+        }
+
+        public override void OnEnd()
+        {
+            enteredCollision = false;
+        }
+
+        public override void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.GetComponent<XRBaseInteractable>())
+                enteredCollision = true;
+        }
+
+
+    }
+}
