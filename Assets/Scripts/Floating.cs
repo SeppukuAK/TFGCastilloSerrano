4211a8958810393452d143e6b96ae0c81016cead
Adriana <Adridelcastillodev@gmail.com>
2020-05-19 14:41:04 +0200

    Movimiento del robot updown y empezando con los materiales de los cart

---
+++ Assets/Scripts/Floating.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Floating : MonoBehaviour
+{
+    public PathType pathSystem = PathType.Linear;
+    public Vector3[] pathval = new Vector3[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOPath(pathval, 6, pathSystem).SetRelative().SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
