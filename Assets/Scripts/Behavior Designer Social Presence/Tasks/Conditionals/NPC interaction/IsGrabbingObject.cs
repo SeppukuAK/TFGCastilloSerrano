0dd8d5aa7232edec105aec7fd3126bf6b8b85502
Raúl Serrano Gómez <raulserranodev@gmail.com>
2020-05-21 09:20:03 +0200

    "Arreglo" de coger y traer objetos y ojos de NPC metidos

---
+++ Assets/Scripts/Behavior Designer Social Presence/Tasks/Conditionals/NPC interaction/IsGrabbingObject.cs
@@ -0,0 +1,26 @@
+﻿using BehaviorDesigner.Runtime.Tasks;
+
+namespace SocialPresenceVR
+{
+    [TaskDescription("Devuelve si el NPC tiene algún objeto agarrado")]
+    [TaskCategory("SocialPresenceVR/NPCInteraction")]
+    public class IsGrabbingObject : Conditional
+    {
+        private SP_NPC NPC;
+
+        /// <summary>
+        /// Obtiene referencias
+        /// </summary>
+        public override void OnAwake()
+        {
+            NPC = GetComponent<SP_NPC>();
+        }
+        public override TaskStatus OnUpdate()
+        {
+            if (NPC.GrabbedInteractable.Interactable != null)
+                return TaskStatus.Success;
+            else
+                return TaskStatus.Failure;
+        }
+    }
+}
