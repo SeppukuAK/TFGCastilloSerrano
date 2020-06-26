9b4002b88dc4b26d216a8038a5d6fcbecc12214b
Raúl Serrano Gómez <raulserranodev@gmail.com>
2020-06-26 18:14:40 +0200

    Ultima versión

---
+++ Assets/Scripts/Behavior Designer Social Presence/Tasks/Conditionals/Player Position/IsPlayerStill.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+
+namespace SocialPresenceVR
+{
+    [TaskDescription("El jugador esta quieto")]
+    [TaskCategory("SocialPresenceVR/PlayerPosition")]
+    [TaskIcon("Assets/Behavior Designer Movement/Editor/Icons/{SkinColor}Play.png")]
+    public class IsPlayerStill : Conditional
+    {
+        /// <summary>
+        /// Referencia al estado de quieto del jugador
+        /// </summary>
+        private PlayerQuietState playerQuietState;
+
+        /// <summary>
+        /// Obtiene referencias
+        /// </summary>
+        public override void OnAwake()
+        {
+            playerQuietState = Camera.main.GetComponentInParent<PlayerQuietState>();
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            if (playerQuietState.Quiet)
+                return TaskStatus.Success;
+            else
+                return TaskStatus.Failure;
+        }
+    }
+}
